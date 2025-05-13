7ff8b38946e63a381a038b548c3330d3954a3a84
Floop <55911687+Floop@users.noreply.github.com>
2025-05-13 22:02:53 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,6 @@ import { Hono } from "jsr:@hono/hono@4.6.5";
 const app = new Hono();
 
 // Respond with "Hi there!" for all requests
-app.get("/", (c) => c.text("Hi there!"));
+app.get("/", (c) => c.json({ message: "Hi again!" }));
 
 export default app;
